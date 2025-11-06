dd49a4bd38af31289d6c9c3cab1ab9fce908bf64
qihy <qihy01@rd.netease.com>
2025-11-06 09:58:56 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+if __name__ == "__main__":
+    print("Hello, World!")
+
+#编写一个加法函数
+def add(a, b):
+    return a + b
+#编写一个减法函数
+def subtract(a, b):
+    return a - b
+
+#编写一个乘法函数
+def multiply(a, b):
+    return a * b
+
+#编写一个除法函数
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero."
+    return a / b
+
+
+
+
+
+
